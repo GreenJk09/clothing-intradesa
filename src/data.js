83f41890c71eb0a461a6fa98bb5ca5802c490b72
GreenJk09 <hkarlos694@gmail.com>
2022-09-25 14:57:08 -0600

    Agregar archivos de proyecto.

---
+++ src/data.js
@@ -0,0 +1,36 @@
+export const sliderItems =[
+    {
+        id: 1,
+        img: "https://i.ibb.co/BcTyyQf/logo-hoodie.png",
+        title: "Discount Logo Hoodie",
+        desc: "Obten está increíble prenda con un precio accesible",
+        bg: "f5fafd"
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/ry5HVvb/monogram-hoodie.png",
+        title: "MONOGRAM HOODIE",
+        desc: "Obten está increíble prenda con un precio accesible",
+        bg: "fcf1ed"
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/hKyWmcH/time-and-true.png",
+        title: "TIME & TRUE",
+        desc: "Obten está increíble prenda con un precio accesible",
+        bg: "fbf0f4"
+    },
+]
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/4MNVpcb/hoodie-style3.jpg",
+        title: "HOODIE STYLES",
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/BVLK9bW/shirt-style1.jpg",
+        title: "SHIRT STYLES",
+    }
+]
